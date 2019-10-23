1fe87a4cce8130d56d2bc1a46d2ae4fc59514c58
Admin <Admin@ADMIN-PC>
2019-10-23 22:33:29 +0200

    Initialising Solution

---
+++ HappyBlazing/Pages/DecoupledConverter.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HappyBlazing.Pages
+{
+    
+    public  class Converter:ComponentBase
+    {
+        public double Kmh = 0;
+        public double Knot = 0;
+        public void Convert()
+        {
+            Knot = Kmh * 0.539957;
+        }
+    }
+}
